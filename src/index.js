218dc2ec0649b41e4c1afe8434a13486438136ec
Arman <armangulistani@gmail.com>
2023-12-24 16:55:41 -0500

    Changed audio functionality
    Now, the audio is not on autoplay because some browsers such as Google Chrome dont allow the autoplay attribute. They force users to click a button or do something to enable the audio.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import "./index.css";
 const texts = [
   "In our homeland's heart, a shadow looms—a relentless foe threatens to plunge us into darkness.",
   "As the chosen defender, you must place your submarines strategically and lead us to victory.",
-  "The fate of our land rests on YOUR shoulders. Will you rise, or will you cower and leave us?",
+  "The fate of our land rests on YOUR shoulders. Will you become a legend, or will you cower and leave us?",
 ];
 const container = document.getElementById("container");
 const muteBtn = document.querySelector(".mute-btn");
 
+// Typewriter function to write letter by letter the each text inside of texts array
 function typeWriter(element, text, i, speed) {
   if (i < text.length) {
     element.textContent += text.charAt(i);
@@ -17,24 +18,33 @@ function typeWriter(element, text, i, speed) {
   }
 }
 
-texts.forEach((text, i) => {
-  setTimeout(
-    () => {
-      const div = document.createElement("div");
-      div.className = "opacity dialogue";
-      typeWriter(div, text, 0, 60); // Adjust speed as needed
-      container.appendChild(div);
-    },
-    i * (text.length * 60 + 2500) // Adjust timing as needed, added 1.5 seconds delay
-  );
-});
+// Start typewriter after 1 second
+setTimeout(() => {
+  texts.forEach((text, i) => {
+    setTimeout(
+      () => {
+        const div = document.createElement("div");
+        div.className = "opacity dialogue";
+        typeWriter(div, text, 0, 60); // Adjust speed as needed
+        container.appendChild(div);
+      },
+      i * (text.length * 60 + 2500) // Adjust timing as needed, added 1.5 seconds delay
+    );
+  });
+}, 1000);
 
 function toggleMute() {
   const audio = document.getElementById("background-music");
-  audio.muted = !audio.muted;
 
-  // Change the icon based on the mute state
-  muteBtn.textContent = audio.muted ? "🔇" : "🔊";
+  if (audio.muted) {
+    audio.muted = false;
+    muteBtn.textContent = "🔊"; // Change the icon to unmuted
+    audio.play(); // Unmute and resume playback
+  } else {
+    audio.muted = true;
+    muteBtn.textContent = "🔇"; // Change the icon to muted
+    audio.pause(); // Mute and pause playback
+  }
 }
 
 muteBtn.addEventListener("click", function () {
